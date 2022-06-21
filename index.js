3ea533cd9856f512f3b949673491630e6cf3c8e7
Krishna Kumar <kkp785216@gmail.com>
2022-06-21 20:14:38 +0530

    folder name changed static to public

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
 const express = require("express");
 const path = require("path");
-const mongoose = require('mongoose');
-const bodyParser = require("body-parser")
-main().catch(err => console.log(err));
-async function main() {
-    await mongoose.connect('mongodb://localhost:27017/krishnaDance');
-}
 const app = express();
 const port = process.env.PORT || 80;
 
-// Define Mongoose schema
-var contactSchema = new mongoose.Schema({
-    name: String,
-    phone: Number,
-    email: String,
-    subject: String,
-    message: String
-});
-
-var Contact = mongoose.model('Contact', contactSchema);
 
 // EXPRESS SPECIFIC STUFF
-app.use('/static', express.static('static')) // For serving static files
+// app.use('/static', express.static('public')) // For serving static files
+app.use('/public', express.static('public')) // For serving static files
+// app.use(express.static('public'))
 
 // PUG SPECIFIC STUFF
 app.set('view engine', 'pug') // Set the template engine as pug
@@ -37,20 +23,6 @@ app.get('/contact', (req, res) => {
     const params = {}
     res.status(200).render('contact.pug', params);
 });
-
-app.post('/contact', (req, res) => {
-    var myData = new Contact(req.body);
-    myData.save().then(()=>{
-        res.send("Thankyou, Your response is saved successfully to the database")
-    }).catch(()=>{
-        res.status(400).send("error in saving the data to the database")
-    })
-})
-
-// app.post('/contact', (req, res) => {
-//     let heee = new Contact(req.body)
-//     console.log(heee);
-// })
 
 
 app.get('/about', (req, res) => {
